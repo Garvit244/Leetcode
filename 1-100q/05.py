e1eeff9f7414cbbe477da6010acf5e1ac0ffa0e3
Garvit244 <garvitbansal244@gmail.com>
2018-06-19 14:49:16 +0800

    Longest Palindrom substring solution in space O(1)

--- 1-100q/05.py
+++ 1-100q/05.py
@@ -48,3 +48,27 @@ class Solution(object):
 
 # Space: O(N^2)
 # Time: O(N^2)
+
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+
+        def expand(s, left, right):
+        	while left >= 0 and right < len(s) and s[left] == s[right]:
+        		left -= 1
+        		right += 1
+        	return right-left-1
+
+        start, end = 0, 0
+        for index in range(len(s)):
+        	even_len = expand(s, index, index+1)
+        	odd_len = expand(s, index, index)
+        	length = max(even_len, odd_len)
+        	if length > (end-start):
+        		start = index - (length-1)/2
+        		end = index +length/2
+
+        return s[start:end+1]
